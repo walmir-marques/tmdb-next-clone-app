3d3fd01006f4a87d7490933d1ed43f37185c404b
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 13:54:41 -0300

    Add About Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
     <div className="flex justify-between items-center p-3 max-w-6xl max-auto">
       <div className="flex gap-4">
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
-        <MenuItem title="Home" address="/" Icon={BsFillInfoCircleFill} />
+        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
       <div className="flex items-center gap-4">
         <DarkModSwitch />
