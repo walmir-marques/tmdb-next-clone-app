8c41432c48adab99c9ef11b0f49f87dcec327dff
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 12:27:41 -0300

    Add Header Section

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center p-3 max-w-6xl max-auto">
+      <div className="flex gap-4">
+        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+        <MenuItem title="Home" address="/" Icon={BsFillInfoCircleFill} />
+      </div>
+      <Link href={"/"} className="flex gap-1 items-center">
+        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounde-lg">
+          IMDb
+        </span>
+        <span className="text-xl hidden sm:inline">Clone</span>
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
