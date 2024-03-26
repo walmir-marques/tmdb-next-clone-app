5c22ffe916d06e746438b9b1a99d535a3cc14beb
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 12:57:55 -0300

    Add Dark Mode using next Themes

---
+++ src/components/DarkModSwitch.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+
+function DarkModSwitch() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  return (
+    <div>
+      {currentTheme === "dark" ? (
+        <MdLightMode
+          onClick={() => setTheme("light")}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      ) : (
+        <MdDarkMode
+          onClick={() => setTheme("dark")}
+          className="text-xl cursor-pointer hover:text-amber-500"
+        />
+      )}
+    </div>
+  );
+}
+
+export default DarkModSwitch;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModSwitch from "./DarkModSwitch";
 
 function Header() {
   return (
@@ -10,12 +11,15 @@ function Header() {
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="Home" address="/" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounde-lg">
-          IMDb
-        </span>
-        <span className="text-xl hidden sm:inline">Clone</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkModSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounde-lg">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </Link>
+      </div>
     </div>
   );
 }
