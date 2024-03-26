f68498964d731f4102262f5db508e90f15a1e312
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 18:48:42 -0300

    add loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-52" src="spinner.svg" alt="Loading ..." />
+    </div>
+  );
+}
+
+export default loading;
