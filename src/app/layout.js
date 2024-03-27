95861be3270b1e8c70fafdb9d55bdd670d9870c9
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 23:32:49 -0300

    suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <Header />
-          <Navbar />
-          <SearchBox />
-          {children}
+          <Suspense>
+            <Header />
+            <Navbar />
+            <SearchBox />
+            {children}
+          </Suspense>
         </Providers>
       </body>
     </html>
