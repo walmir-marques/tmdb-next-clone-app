5c515155260ecb7ad653abc8b3453541147f776c
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-26 23:43:56 -0300

    error fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 function Navbar() {
   return (
     <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-      <NavbarItem title="Em Alta" param="fetchTrending" />
-      <NavbarItem title="Melhores Avaliados" param="fetchTopRated" />
+      <Suspense>
+        <NavbarItem title="Em Alta" param="fetchTrending" />
+        <NavbarItem title="Melhores Avaliados" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 }
